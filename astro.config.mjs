56777c1f4ea665a21fd0c038d25e983c11566830
Reilly Vejar <93952071+reillyvejar@users.noreply.github.com>
2024-06-03 12:01:31 -0400

    New bare bones website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import deno from "freestyle-deno-astro-adapter";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: deno(),
-  integrations: [svelte(), react()],
+  integrations: [svelte(), react(), tailwind()],
   output: "server",
   vite: {
     ssr: {
