eb606ceb5ba649d76f1be7ac67b7ac64c010dbac
Reilly Vejar <rdvej@104-0-35-187.lightspeed.chtnsc.sbcglobal.net>
2024-06-25 06:18:47 -0400

    Fixing repo bugs

---
+++ .freestyle/freestyle.config.js
@@ -0,0 +1,7 @@
+// freestyle.config.ts
+import { defineConfig } from "freestyle-sh";
+var freestyle_config_default = defineConfig({});
+export {
+  freestyle_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vZnJlZXN0eWxlLmNvbmZpZy50cyJdLAogICJzb3VyY2VzQ29udGVudCI6IFsiaW1wb3J0IHsgZGVmaW5lQ29uZmlnIH0gZnJvbSBcImZyZWVzdHlsZS1zaFwiO1xuXG5leHBvcnQgZGVmYXVsdCBkZWZpbmVDb25maWcoe30pOyJdLAogICJtYXBwaW5ncyI6ICI7QUFBQSxTQUFTLG9CQUFvQjtBQUU3QixJQUFPLDJCQUFRLGFBQWEsQ0FBQyxDQUFDOyIsCiAgIm5hbWVzIjogW10KfQo=
